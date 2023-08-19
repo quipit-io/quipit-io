0efd28cc1938a96948cea7dc54ab3b1dea855edf
michael.gearhardt@fathym.com <michael.gearhardt@fathym.com>
2023-08-19 23:46:12 +0000

    "Refactor index.tsx: Replace className with class, <a /> with <Action />, and style Actions with tailwindcss"

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,25 +1,26 @@
 import { MenuIcon } from "$fathym/atomic-icons";
+import { Action } from "components";
 
 export default async function Home() {
   return (
-    <div className="px-4 py-8 mx-auto bg-[#86efac]">
-      <div className="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold text-white mt-8">Welcome to SportsHub</h1>
-        <p className="text-lg text-white mt-4">
+    <div class="px-4 py-8 mx-auto bg-[#86efac]">
+      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+        <h1 class="text-4xl font-bold text-white mt-8">Welcome to SportsHub</h1>
+        <p class="text-lg text-white mt-4">
           The ultimate platform for sports writers, players, and teams to share, collaborate, and discuss.
         </p>
-        <div className="flex flex-col items-center mt-8">
-          <a href="/oauth/signin/github" className="btn btn-github">
+        <div class="flex flex-col items-center mt-8">
+          <Action href="/oauth/signin/github" class="btn btn-github">
             Sign in with GitHub
-          </a>
-          <a href="/oauth/signin/google" className="btn btn-google">
+          </Action>
+          <Action href="/oauth/signin/google" class="btn btn-google">
             Sign in with Google
-          </a>
-          <a href="/oauth/signin/microsoft" className="btn btn-microsoft">
+          </Action>
+          <Action href="/oauth/signin/microsoft" class="btn btn-microsoft">
             Sign in with Microsoft
-          </a>
+          </Action>
         </div>
-        <p className="text-sm text-white mt-4">
+        <p class="text-sm text-white mt-4">
           We use social logins to ensure the security of your account.
         </p>
       </div>
