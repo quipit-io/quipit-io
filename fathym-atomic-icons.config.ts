e1e0182fed5d8fbc325dec99659892d618c4537f
michael.gearhardt@fathym.com <michael.gearhardt@fathym.com>
2023-08-20 00:27:06 +0000

    "Added GitHub, Google, and Microsoft icons to IconMap object"

--- fathym-atomic-icons.config.ts
+++ fathym-atomic-icons.config.ts
@@ -2,17 +2,16 @@ import { IconSetConfig, IconSetGenerateConfig } from "@fathym/atomic-icons";
 
 export const curIconSetConfig: IconSetConfig = {
   IconMap: {
-    "chevron-right":
-      "https://api.iconify.design/material-symbols:chevron-right-rounded.svg",
+    "chevron-right": "https://api.iconify.design/material-symbols:chevron-right-rounded.svg",
     "chevron-down": "https://api.iconify.design/mdi:chevron-down.svg",
-    "emulated-device":
-      "https://api.iconify.design/material-symbols:android-find-my-device-outline.svg",
-    "connected-devices":
-      "https://api.iconify.design/material-symbols:cast-connected-outline.svg",
-    "device-telemetry":
-      "https://api.iconify.design/material-symbols:query-stats.svg",
+    "emulated-device": "https://api.iconify.design/material-symbols:android-find-my-device-outline.svg",
+    "connected-devices": "https://api.iconify.design/material-symbols:cast-connected-outline.svg",
+    "device-telemetry": "https://api.iconify.design/material-symbols:query-stats.svg",
     menu: "https://api.iconify.design/ci:hamburger.svg",
     user: "https://api.iconify.design/material-symbols:account-circle-full.svg",
+    github: "https://api.iconify.design/mdi:github.svg",
+    google: "https://api.iconify.design/mdi:google.svg",
+    microsoft: "https://api.iconify.design/mdi:microsoft.svg"
   },
 };
 
